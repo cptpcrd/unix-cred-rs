2ff7c2231e2a783a1808261ca73caa3acb35cc16
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2021-06-26 13:40:38 -0400

    feat: add macOS interface to get peer PID
    And document the ways in which it differs from other OSes.

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,7 @@
 //!
 //! The `ucred` and `xucred` modules expose the OS-specific interfaces. `ucred` provides the
 //! Linux/OpenBSD/NetBSD interface, and `xucred` provides the macOS/FreeBSD/DragonFlyBSD interface.
+//! `get_peerpid()` also exposes a macOS-specific interface to get the PID.
 //!
 //! `ucred` is not particularly useful; in most cases you should use `get_peer_ids()` or
 //! `get_peer_pid_ids()`, which are more cross-platform. However, `xucred` can be helpful since it
@@ -43,6 +44,32 @@ pub mod ucred;
     target_os = "ios"
 ))]
 pub mod xucred;
+
+#[cfg(any(target_os = "macos", target_os = "ios"))]
+#[inline]
+unsafe fn get_peerpid_raw(sockfd: RawFd) -> io::Result<libc::pid_t> {
+    let mut pid = 0;
+    crate::util::getsockopt_raw(
+        sockfd,
+        libc::SOL_LOCAL,
+        libc::LOCAL_PEERPID,
+        std::slice::from_mut(&mut pid),
+    )?;
+    Ok(pid)
+}
+
+/// Get the PID of the given socket's peer.
+///
+/// This is only available on macOS. [`get_peer_pid_ids()`] should be used instead in most cases
+/// since it is cross-platform.
+///
+/// Unlike with other platforms, the PID returned by this function is the PID of the process that
+/// last accessed the socket.
+#[cfg(any(target_os = "macos", target_os = "ios"))]
+#[inline]
+pub fn get_peerpid(sock: &UnixStream) -> io::Result<libc::pid_t> {
+    unsafe { get_peerpid_raw(sock.as_raw_fd()) }
+}
 
 #[allow(clippy::needless_return)]
 #[inline]
@@ -76,6 +103,8 @@ pub fn get_peer_ids(sock: &UnixStream) -> io::Result<(libc::uid_t, libc::gid_t)>
     target_os = "openbsd",
     target_os = "netbsd",
     target_os = "freebsd",
+    target_os = "macos",
+    target_os = "ios",
 ))]
 #[allow(clippy::needless_return)]
 #[inline]
@@ -93,20 +122,34 @@ unsafe fn get_peer_pid_ids_raw(
         let cred = xucred::get_xucred_raw(sockfd)?;
         return Ok((cred.pid(), cred.uid(), cred.gid()));
     }
+
+    #[cfg(any(target_os = "macos", target_os = "ios"))]
+    {
+        let cred = xucred::get_xucred_raw(sockfd)?;
+        let pid = get_peerpid_raw(sockfd)?;
+        return Ok((Some(pid), cred.uid(), cred.gid()));
+    }
 }
 
 /// Get the PID, UID, and GID of the given socket's peer.
 ///
-/// This only works on Linux, OpenBSD, NetBSD, and FreeBSD 13+. On other operating systems, this
-/// function is not available. On FreeBSD 12 and earlier, the returned PID is always `None`.
+/// This only works on Linux, OpenBSD, NetBSD, FreeBSD 13+, and macOS/iOS. On other operating
+/// systems, this function is not available. On FreeBSD 12 and earlier, the returned PID is always
+/// `None`.
 ///
-/// **WARNING**: The returned PID is the PID of the process that originally opened the socket. That
-/// process may have died, and another process may now be running with that PID. Use with caution.
+/// **WARNING**: On most platforms (currently, the only exception is macOS), the returned PID is
+/// the PID of the process that originally opened the socket. That process may have died, and
+/// another process may now be running with that PID. Use with caution.
+///
+/// On macOS, the returned PID is the PID of the process that last accessed the socket. However,
+/// this still presents race conditions. Use carefully.
 #[cfg(any(
     target_os = "linux",
     target_os = "openbsd",
     target_os = "netbsd",
     target_os = "freebsd",
+    target_os = "macos",
+    target_os = "ios",
 ))]
 #[inline]
 pub fn get_peer_pid_ids(
@@ -118,6 +161,53 @@ pub fn get_peer_pid_ids(
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[cfg(any(target_os = "macos", target_os = "ios"))]
+    #[test]
+    fn test_get_peerpid() {
+        let (a, b) = UnixStream::pair().unwrap();
+
+        let apid = get_peerpid(&a).unwrap();
+        assert_eq!(apid, unsafe { libc::getpid() });
+
+        let bpid = get_peerpid(&b).unwrap();
+        assert_eq!(bpid, unsafe { libc::getpid() });
+    }
+
+    #[cfg(any(target_os = "macos", target_os = "ios"))]
+    #[test]
+    fn test_get_peerpid_bad_fd() {
+        assert_eq!(
+            get_peerpid(unsafe { &UnixStream::from_raw_fd(libc::c_int::MAX) })
+                .unwrap_err()
+                .raw_os_error(),
+            Some(libc::EBADF),
+        );
+
+        let file = std::fs::File::open(std::env::current_exe().unwrap()).unwrap();
+        assert_eq!(
+            get_peerpid(unsafe { &UnixStream::from_raw_fd(file.into_raw_fd()) })
+                .unwrap_err()
+                .raw_os_error(),
+            Some(libc::ENOTSOCK),
+        );
+    }
+
+    #[cfg(any(target_os = "macos", target_os = "ios"))]
+    #[test]
+    fn test_get_peerpid_error() {
+        use std::os::unix::net::UnixDatagram;
+
+        let dir = tempfile::tempdir().unwrap();
+
+        let sock = UnixDatagram::bind(dir.path().join("sock1")).unwrap();
+        assert_eq!(
+            get_peerpid(unsafe { &UnixStream::from_raw_fd(sock.into_raw_fd()) })
+                .unwrap_err()
+                .raw_os_error(),
+            Some(libc::ENOTCONN),
+        );
+    }
 
     #[test]
     fn test_get_peer_ids() {
@@ -155,6 +245,8 @@ mod tests {
         target_os = "openbsd",
         target_os = "netbsd",
         target_os = "freebsd",
+        target_os = "macos",
+        target_os = "ios",
     ))]
     #[test]
     fn test_get_peer_pid_ids() {
@@ -176,6 +268,8 @@ mod tests {
         target_os = "openbsd",
         target_os = "netbsd",
         target_os = "freebsd",
+        target_os = "macos",
+        target_os = "ios",
     ))]
     #[test]
     fn test_get_peer_pid_ids_bad_fd() {
@@ -200,6 +294,8 @@ mod tests {
         target_os = "openbsd",
         target_os = "netbsd",
         target_os = "freebsd",
+        target_os = "macos",
+        target_os = "ios",
     ))]
     #[allow(clippy::unnecessary_wraps)]
     fn get_expected_pid() -> Option<libc::pid_t> {
